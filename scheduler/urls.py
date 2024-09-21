5d23501c861da05a9f6357876fba58f6c7b260f1
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2024-09-22 00:22:59 +0300

    lu

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import ScheduleView, AboutView, available_slots_api, BarberShopDetailView, BarberShopBookingView, RegisterView
+from .views import ScheduleView, AboutView, available_slots_api, BarberShopDetailView, BarberShopBookingView, RegisterView, LoginView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register_business'),  # Specific path for registration
+    path('register/', RegisterView.as_view(), name='register_business'),  # Path for registration
+    path('login/', LoginView.as_view(), name='login'),  # Add this line for the login view with the correct name
     path('', ScheduleView.as_view(), name='schedule'),
     path('about/', AboutView.as_view(), name='about'),
     path('available-slots/<str:name>/', available_slots_api.as_view(), name='available-slots'),
     path('<str:name>/', BarberShopDetailView.as_view(), name='barbershop_detail'),
     path('<str:name>/book/', BarberShopBookingView.as_view(), name='barbershop_booking'),
-
-    # Other URL patterns...
 ]
