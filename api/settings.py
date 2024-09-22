d8ac1cc082782166e62b42a0ccb762eec65ed804
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2024-09-22 23:40:39 +0300

    gj

--- api/settings.py
+++ api/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'phonenumber_field',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -51,10 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     ...
 ]
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
