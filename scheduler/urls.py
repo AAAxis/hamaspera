644b2ddbbddab3bccb57d1f6825a9d3d9b9c7687
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2024-09-25 02:11:15 +0300

    vom

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     path('', ScheduleView.as_view(), name='schedule'),
     path('about/', AboutView.as_view(), name='about'),
     path('available-slots/<str:name>/', available_slots_api.as_view(), name='available-slots'),
-    path('<str:name>/', BarberShopDetailView.as_view(), name='barbershop_detail'),
-    path('<str:name>/book/', BarberShopBookingView.as_view(), name='barbershop_booking'),
+    path('barbershop/<str:id>/', BarberShopDetailView.as_view(), name='barbershop_detail'),
+    path('barbershop/<str:name>/book/', BarberShopBookingView.as_view(), name='barbershop_booking'),
+
    
 ]
