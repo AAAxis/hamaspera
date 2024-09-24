0a6798c36dc265158f3a03276dc5c693f63b519c
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2024-09-24 14:54:45 +0300

    cors

--- api/settings.py
+++ api/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -54,11 +55,8 @@ MIDDLEWARE = [
     
 ]
 
-# OR specify allowed origins (for production)
-CORS_ALLOWED_ORIGINS = [
-    "https://hamaspera.vercel.app",
-
-]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'api.urls'
 
